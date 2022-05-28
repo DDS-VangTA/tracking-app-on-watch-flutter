8b28e51c46fb1af3cd0ab22d2cac6fec768d86f0
DDS-VangTA <vang.thaoa@ddsolution.vn>
2022-05-28 14:46:28 +0700

    implement UI, add lib for wearOS

--- tracking_app_on_watch_flutter/android/app/src/main/kotlin/com/example/tracking_app_on_watch_flutter/MainActivity.kt
+++ tracking_app_on_watch_flutter/android/app/src/main/kotlin/com/example/tracking_app_on_watch_flutter/MainActivity.kt
@@ -1,3 +1,29 @@
+//package com.example.tracking_app_on_watch_flutter
+//
+//import android.os.Bundle
+//
+//import io.flutter.app.FlutterActivity
+//import io.flutter.plugins.GeneratedPluginRegistrant
+//
+//import androidx.wear.ambient.AmbientMode
+//
+//import com.example.tracking_app_on_watch_flutter.FlutterAmbientCallback
+//import com.example.tracking_app_on_watch_flutter.getChannel
+//
+//class MainActivity: FlutterActivity(), AmbientMode.AmbientCallbackProvider {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        GeneratedPluginRegistrant.registerWith(this)
+//
+//        // Wire up the activity for ambient callbacks
+//        AmbientMode.attachAmbientSupport(this)
+//    }
+//
+//    override fun getAmbientCallback(): AmbientMode.AmbientCallback {
+//        return FlutterAmbientCallback(getChannel(flutterView))
+//    }
+//}
+
 package com.example.tracking_app_on_watch_flutter
 
 import io.flutter.embedding.android.FlutterActivity
--- tracking_app_on_watch_flutter/lib/main.dart
+++ tracking_app_on_watch_flutter/lib/main.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:tracking_app_on_watch_flutter/view/home_page.dart';
-import 'package:tracking_app_on_watch_flutter/viewmodel/location_view_model.dart';
+import 'package:wear/wear.dart';
 
-void main() {
-  runApp(MultiProvider(
-    providers: [ChangeNotifierProvider(create: (_) => LocationViewModel())],
-    child: Builder(builder: (context) {
-      return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        home: HomePage(),
-      );
-    }),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: WatchShape(
+            builder: (BuildContext context, WearShape shape, Widget? child) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(Icons.arrow_back_ios),
+                  ),
+                  Text(
+                    'Shape: ${shape == WearShape.round ? 'round' : 'square'}',
+                  ),
+                  child!,
+                ],
+              );
+            },
+            child: AmbientMode(
+              builder: (BuildContext context, WearMode mode, Widget? child) {
+                return Text(
+                  'Mode: ${mode == WearMode.active ? 'Active' : 'Ambient'}',
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// import 'package:tracking_app_on_watch_flutter/view/home_page.dart';
+// import 'package:tracking_app_on_watch_flutter/viewmodel/location_view_model.dart';
+//
+// void main() {
+//   runApp(MultiProvider(
+//     providers: [ChangeNotifierProvider(create: (_) => LocationViewModel())],
+//     child: Builder(builder: (context) {
+//       return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: 'Flutter Demo',
+//         home: HomePage(),
+//       );
+//     }),
+//   ));
+// }
